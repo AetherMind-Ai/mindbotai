22693645c352e51c1eaff71d834f1ba3b9ab56e3
AetherMind-Ai <clashinkofsr@gmail.com>
2024-11-20 14:03:23 +0200

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { FcGoogle } from 'react-icons/fc'
+import React from "react";
+import { TbMessageChatbot } from "react-icons/tb"; // Chatbot with face
 
 const Loading = () => {
   return (
-    <section className='h-full w-full grid place-content-center'>
-      <div className='flex items-center justify-center gap-3 text-8xl'>
-      <FcGoogle />
+    <section className="h-full w-full grid place-content-center">
+      <div className="flex items-center justify-center gap-3 text-8xl">
+        <TbMessageChatbot />
       </div>
       <div className="loader" />
     </section>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
